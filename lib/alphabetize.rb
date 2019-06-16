9e059526e95e9b0bb743f8840ff903fa5e2af703
Amanda Treutler <amandathedev@gmail.com>
2019-06-16 13:22:11 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
 def alphabetize(arr)
-  # code here
+  esperanto = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  arr.sort_by do |text|
+    text.split('').map do |char|
+      esperanto.index(char)
+    end
+  end
 end
+
+arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+alphabetize(arr)
